e2bda647a1abbb91069da1e54d24822ac0ad1b51
Yutaka Shimada <shimayu22@gmail.com>
2019-11-01 13:09:12 +0900

    players.py(選手情報Model)を追加

---
+++ eikan/models/players.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+
+# Create your models here.
+class Players(models.Model):
+
+    # TODO: ポジションを作成する
+    POSITION_CHOICES = (
+        (0, "選択"),
+        (1, "投"),
+        (2, "捕"),
+    )
+
+    admission_year = models.IntegerField(
+        verbose_name = "入学年度",
+        validators = [MinValueValidator(1939)],
+        # TODO: 一つ前のレコードの年度をdefaultにしたい
+    )
+
+    name = models.CharField(
+        verbose_name = "名前",
+        max_length = 6,
+    )
+
+    position = models.IntegerField(
+        verbose_name = "守備位置",
+        choices = POSITION_CHOICES,
+        default = 0,
+    )
+
+    remark = models.CharField(
+        verbose_name = "備考",
+        max_length = 100,
+        blank = True,
+        null = True,
+    )
+
+    created_at = models.DateTimeField(
+        verbose_name = "登録日",
+        auto_now_add = True,
+    )
+
+    updated_at = models.DateTimeField(
+        verbose_name = "登録日",
+        auto_now_add = True,
+    )
+
+    class Meta:
+        verbose_name_plural = "選手情報"

ba23b2ba73dc1299f806f84317177925c5cb824d
Yutaka Shimada <shimayu22@gmail.com>
2019-11-01 15:20:03 +0900

    モデルを有効にする

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eikan.apps.EikanConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

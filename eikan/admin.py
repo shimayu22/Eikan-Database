4879abf89258f5a4ba97a4b61ed11cbc83d7425b
Yutaka Shimada <shimayu22@gmail.com>
2019-11-05 11:26:17 +0900

    野手成績と投手成績のカラムの幅を小さくした

--- eikan/admin.py
+++ eikan/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import NumberInput
 
 # Register your models here.
 from .models import Teams, Players, Games, Fielder_results, Pitcher_results
@@ -7,11 +9,17 @@ class Fielder_resultsInline(admin.TabularInline):
     model = Fielder_results
     verbose_name = "野手"
     extra = 9
+    formfield_overrides = {
+        models.PositiveSmallIntegerField: {'widget': NumberInput(attrs={'style':'width: 2.5em;'})}
+    }
 
 class Pitcher_resultsInline(admin.TabularInline):
     model = Pitcher_results
     verbose_name = "投手"
     extra = 1
+    formfield_overrides = {
+        models.PositiveSmallIntegerField: {'widget': NumberInput(attrs={'style':'width: 2.5em;'})}
+    }
 
 class TeamsAdmin(admin.ModelAdmin):
     list_display = ('year', 'period', 'prefecture', 'training_policy', 'draft_nomination', 'remark')

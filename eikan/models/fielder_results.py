b2e1e4a4b3e7722713552eef07b1f2c113ace2fe
Yutaka Shimada <shimayu22@gmail.com>
2019-11-11 11:28:12 +0900

    微調整

--- eikan/models/fielder_results.py
+++ eikan/models/fielder_results.py
@@ -20,94 +20,94 @@ class Fielder_results(models.Model):
     player_id = models.ForeignKey(
         Players,
         on_delete=models.CASCADE,
-        verbose_name = "選手",
-        limit_choices_to = {"admission_year__range": \
-                            (start_year, finish_year)},
+        verbose_name="選手",
+        limit_choices_to={"admission_year__range": \
+                          (start_year, finish_year)},
     )
 
     at_bat = models.PositiveSmallIntegerField(
-        verbose_name = "打数",
-        default = 0,
+        verbose_name="打数",
+        default=0,
     )
 
     run = models.PositiveSmallIntegerField(
-        verbose_name = "得点",
-        default = 0,
+        verbose_name="得点",
+        default=0,
     )
 
     hit = models.PositiveSmallIntegerField(
-        verbose_name = "安打",
-        default = 0,
+        verbose_name="安打",
+        default=0,
     )
 
     two_base = models.PositiveSmallIntegerField(
-        verbose_name = "二塁打",
-        default = 0,
+        verbose_name="二塁打",
+        default=0,
     )
 
     three_base = models.PositiveSmallIntegerField(
-        verbose_name = "三塁打",
-        default = 0,
+        verbose_name="三塁打",
+        default=0,
     )
 
     home_run = models.PositiveSmallIntegerField(
-        verbose_name = "本塁打",
-        default = 0,
+        verbose_name="本塁打",
+        default=0,
     )
 
     run_batted_in = models.PositiveSmallIntegerField(
-        verbose_name = "打点",
-        default = 0,
+        verbose_name="打点",
+        default=0,
     )
 
     strike_out = models.PositiveSmallIntegerField(
-        verbose_name = "三振",
-        default = 0,
+        verbose_name="三振",
+        default=0,
     )
 
     bases_on_balls = models.PositiveSmallIntegerField(
-        verbose_name = "四球",
-        default = 0,
+        verbose_name="四球",
+        default=0,
     )
 
     hit_by_pitch = models.PositiveSmallIntegerField(
-        verbose_name = "死球",
-        default = 0,
+        verbose_name="死球",
+        default=0,
     )
 
     sacrifice_bunt = models.PositiveSmallIntegerField(
-        verbose_name = "犠打",
-        default = 0,
+        verbose_name="犠打",
+        default=0,
     )
 
     sacrifice_fly = models.PositiveSmallIntegerField(
-        verbose_name = "犠飛",
-        default = 0,
+        verbose_name="犠飛",
+        default=0,
     )
 
     stolen_base = models.PositiveSmallIntegerField(
-        verbose_name = "盗塁",
-        default = 0,
+        verbose_name="盗塁",
+        default=0,
     )
 
     grounded_into_double_play = models.PositiveSmallIntegerField(
-        verbose_name = "併殺",
-        default = 0,
+        verbose_name="併殺",
+        default=0,
     )
 
     error = models.PositiveSmallIntegerField(
-        verbose_name = "失策",
-        default = 0,
+        verbose_name="失策",
+        default=0,
     )
 
     created_at = models.DateTimeField(
-        verbose_name = "登録日",
-        auto_now_add = True,
+        verbose_name="登録日",
+        auto_now_add=True,
     )
 
     updated_at = models.DateTimeField(
-        verbose_name = "更新日",
-        auto_now = True,
+        verbose_name="更新日",
+        auto_now=True,
     )
 
     def __str__(self):
--- eikan/models/pitcher_results.py
+++ eikan/models/pitcher_results.py
@@ -21,97 +21,97 @@ class Pitcher_results(models.Model):
     player_id = models.ForeignKey(
         Players,
         on_delete=models.CASCADE,
-        verbose_name = "選手",
-        limit_choices_to = {"admission_year__range": \
-                            (start_year, finish_year), \
-                            "position": 1   },
+        verbose_name="選手",
+        limit_choices_to={"admission_year__range": \
+                           (start_year, finish_year), \
+                           "position": 1},
     )
 
     games_started = models.BooleanField(
-        verbose_name = "先発登板",
-        default = False,
+        verbose_name="先発登板",
+        default=False,
     )
 
     innings_pitched = models.PositiveSmallIntegerField(
-        verbose_name = "イニング",
-        validators = [MinValueValidator(0),MaxValueValidator(15)],
-        default = 0,
+        verbose_name="イニング",
+        validators=[MinValueValidator(0),MaxValueValidator(15)],
+        default=0,
     )
 
     innings_pitched_fraction = models.PositiveSmallIntegerField(
-        verbose_name = "/3",
-        validators = [MinValueValidator(0),MaxValueValidator(2)],
-        default = 0,
+        verbose_name="/3",
+        validators=[MinValueValidator(0),MaxValueValidator(2)],
+        default=0,
     )
 
     total_batters_faced = models.PositiveSmallIntegerField(
-        verbose_name = "対戦打者",
-        default = 0,
+        verbose_name="対戦打者",
+        default=0,
     )
 
     number_of_pitch = models.PositiveSmallIntegerField(
-        verbose_name = "投球数",
-        default = 0,
+        verbose_name="投球数",
+        default=0,
     )
 
     hit = models.PositiveSmallIntegerField(
-        verbose_name = "被安打",
-        default = 0,
+        verbose_name="被安打",
+        default=0,
     )
 
     strike_out = models.PositiveSmallIntegerField(
-        verbose_name = "三振",
-        default = 0,
+        verbose_name="三振",
+        default=0,
     )
 
     bases_on_balls = models.PositiveSmallIntegerField(
-        verbose_name = "四球",
-        default = 0,
+        verbose_name="四球",
+        default=0,
     )
 
     hit_by_pitch = models.PositiveSmallIntegerField(
-        verbose_name = "死球",
-        default = 0,
+        verbose_name="死球",
+        default=0,
     )
 
     run = models.PositiveSmallIntegerField(
-        verbose_name = "失点",
-        default = 0,
+        verbose_name="失点",
+        default=0,
     )
 
     earned_run = models.PositiveSmallIntegerField(
-        verbose_name = "自責点",
-        default = 0,
+        verbose_name="自責点",
+        default=0,
     )
 
     wild_pitch = models.PositiveSmallIntegerField(
-        verbose_name = "暴投",
-        default = 0,
+        verbose_name="暴投",
+        default=0,
     )
 
     home_run = models.PositiveSmallIntegerField(
-        verbose_name = "本塁打",
-        default = 0,
+        verbose_name="本塁打",
+        default=0,
     )
 
     sacrifice_bunt = models.PositiveSmallIntegerField(
-        verbose_name = "犠打",
-        default = 0,
+        verbose_name="犠打",
+        default=0,
     )
 
     sacrifice_fly = models.PositiveSmallIntegerField(
-        verbose_name = "犠飛",
-        default = 0,
+        verbose_name="犠飛",
+        default=0,
     )
 
     created_at = models.DateTimeField(
-        verbose_name = "登録日",
-        auto_now_add = True,
+        verbose_name="登録日",
+        auto_now_add=True,
     )
 
     updated_at = models.DateTimeField(
-        verbose_name = "更新日",
-        auto_now = True,
+        verbose_name="更新日",
+        auto_now=True,
     )
 
     def __str__(self):

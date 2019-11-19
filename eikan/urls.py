69d852f3370e612a00f8b0af80510c4d8a11e292
Yutaka Shimada <shimayu22@gmail.com>
2019-11-19 13:05:54 +0900

    作成予定のviewをurlsに追加

--- eikan/urls.py
+++ eikan/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # /eikan/
     path('', views.index, name='index'),
+    # チーム一覧 /eikan/team/
+    path('team', views.TeamView.as_view(), name='teams'),
+    # チーム詳細 /eikan/team/id/
+    path('team/<int:pk>/', views.TeamDetailView.as_view(), name='team_detail'),
+    # 選手一覧 /eikan/player/
+    path('player', views.PlayerView.as_view(), name='player'),
+    # 選手詳細 /eikan/player/id
+    path('player/<int:pk>/', views.PlayerDetailView.as_view(), name='player_detail'),
 ]

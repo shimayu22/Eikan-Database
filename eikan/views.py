c1e3582434541683402d829a97b55b4591c6380d
Yutaka Shimada <shimayu22@gmail.com>
2019-12-11 22:49:47 +0900

    indexにチーム成績を表示

--- eikan/views.py
+++ eikan/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-from .models import Teams, Players ,TeamsTotalResults ,FielderTotalResults
+from .models import Teams, Players, TeamsTotalResults, \
+                    FielderTotalResults, PitcherTotalResults
 
 # Create your views here.
-class IndexView(generic.ListView):
+class IndexView(generic.TemplateView):
     template_name = 'eikan/index.html'
-    context_object_name = 'team_list'
 
-    def get_queryset(self):
-        return Teams.objects.order_by('-year', '-period')[:3]
-        #return TeamsTotalResults.order_by('team_id')[:3]
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['team_total_result'] = TeamsTotalResults.objects.latest('pk')
+        start_year = (ctx['team_total_result'].year - 2) if ctx['team_total_result'].period == 1 else (ctx['team_total_result'].year - 1)
+        players = Players.objects.filter(admission_year__gte=start_year, admission_year__lte=ctx['team_total_result'].year)
+        pitchers = Players.objects.filter(admission_year__gte=start_year, admission_year__lte=ctx['team_total_result'].year, is_pitcher=True)
+        ctx['fielder_total_results'] = FielderTotalResults.objects.filter(player_id__in=players)
+        ctx['pitcher_total_results'] = PitcherTotalResults.objects.filter(player_id__in=pitchers)
+
+        return ctx
 
 class TeamView(generic.ListView):
     template_name = 'eikan/team.html'

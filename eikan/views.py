a9d26c5d1f57bb1758557d6e7c20853321818e08
Yutaka Shimada <shimayu22@gmail.com>
2020-01-13 16:19:27 +0900

    ListViewだとクエリ数が多いので、TemplateViewにしてクエリを減らした

--- eikan/views.py
+++ eikan/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_list_or_404
-from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic import TemplateView, DetailView
 from eikan import sabr_manager as s
 
 from .models import Teams, Players, Games, \
@@ -41,10 +41,15 @@ class IndexView(TemplateView):
         return ctx
 
 
-class TeamView(ListView):
-    model = TeamTotalResults
+class TeamView(TemplateView):
     template_name = 'eikan/teams.html'
-    context_object_name = 'team_total_results'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['team_total_results'] = TeamTotalResults.objects.select_related(
+            'team_id').all().order_by('team_id')
+
+        return ctx
 
 
 class TeamDetailView(DetailView):
@@ -65,38 +70,44 @@ class TeamDetailView(DetailView):
         player_list = fielder_results.values('player_id').distinct()
         # <QuerySet [{'player_id': 1}, {'player_id': 2}, {'player_id': 3}, {'player_id': 4}]>
         # 選手ごとにこのチームだった時の指標を計算する
-        if fielder_results.exists():
-            for f in player_list:
-                sfs = s.FielderSabrManager(
-                    f['player_id'], fielder_results.filter(
-                        player_id=f['player_id']))
-                ctx['fielder_results'].append(sfs.create_sabr_from_results())
+        for f in player_list:
+            sfs = s.FielderSabrManager(
+                f['player_id'], fielder_results.filter(player_id=f['player_id']))
+            ctx['fielder_results'].append(sfs.create_sabr_from_results())
 
         # 投手編
         ctx['pitcher_results'] = []
         pitcher_results = PitcherResults.objects.select_related(
             'player_id').filter(game_id__team_id=teams).order_by('player_id')
         pitcher_list = pitcher_results.values('player_id').distinct()
-        if pitcher_results.exists():
-            for p in pitcher_list:
-                sfs = s.PitcherSabrManager(
-                    p['player_id'], pitcher_results.filter(
-                        player_id=p['player_id']))
-                ctx['pitcher_results'].append(sfs.create_sabr_from_results())
+        for p in pitcher_list:
+            sfs = s.PitcherSabrManager(
+                p['player_id'], pitcher_results.filter(player_id=p['player_id']))
+            ctx['pitcher_results'].append(sfs.create_sabr_from_results())
 
         return ctx
 
 
-class FielderView(ListView):
-    model = FielderTotalResults
+class FielderView(TemplateView):
     template_name = 'eikan/fielders.html'
-    context_object_name = 'fielder_total_results'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['fielder_total_results'] = FielderTotalResults.objects.select_related(
+            'player_id').all().order_by('player_id')
+
+        return ctx
 
 
-class PitcherView(ListView):
-    model = PitcherTotalResults
+class PitcherView(TemplateView):
     template_name = 'eikan/pitchers.html'
-    context_object_name = 'pitcher_total_results'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['pitcher_total_results'] = PitcherTotalResults.objects.select_related(
+            'player_id').all().order_by('player_id')
+
+        return ctx
 
 
 class PlayerDetailView(DetailView):
@@ -108,11 +119,11 @@ class PlayerDetailView(DetailView):
 
         ctx = super().get_context_data(**kwargs)
         # 打者総合成績を取得（投手野手共通）
-        ctx['fielder_total_results'] = FielderTotalResults.objects.get(
-            player_id=player)
+        ctx['fielder_total_results'] = FielderTotalResults.objects.select_related(
+            'player_id').get(player_id=player)
         # 1年生時の西暦から、3年夏までの試合結果を取得する
         ctx['fielder_results'] = FielderResults.objects.select_related(
-            'game_id__team_id').filter(player_id=player)
+            'game_id__team_id', 'game_id', 'player_id').filter(player_id=player)
         # ctx["fielder_results_n"] n=1～3
         for i in range(0, 3):
             key = "fielder_results_" + str(i + 1)
@@ -125,10 +136,10 @@ class PlayerDetailView(DetailView):
 
         # 投手のみ以下の処理を行う
         if player.is_pitcher:
-            ctx['pitcher_total_results'] = PitcherTotalResults.objects.get(
-                player_id=player)
+            ctx['pitcher_total_results'] = PitcherTotalResults.objects.select_related(
+                'player_id').get(player_id=player)
             ctx['pitcher_results'] = PitcherResults.objects.select_related(
-                'game_id__team_id').filter(player_id=player)
+                'game_id__team_id', 'game_id', 'player_id').filter(player_id=player)
             for i in range(0, 3):
                 key = "pitcher_results_" + str(i + 1)
                 ctx[key] = []
@@ -141,10 +152,15 @@ class PlayerDetailView(DetailView):
         return ctx
 
 
-class GameView(ListView):
-    model = Games
+class GameView(TemplateView):
     template_name = 'eikan/games.html'
-    context_object_name = 'games'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['games'] = Games.objects.select_related(
+            'team_id').all().order_by('team_id')
+
+        return ctx
 
 
 class GameDetailView(DetailView):

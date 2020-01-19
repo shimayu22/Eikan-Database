e3c8c33d16021baeb78944c204649c23e2ffc0e2
Yutaka Shimada <shimayu22@gmail.com>
2020-01-19 17:32:26 +0900

    過去の試合結果を修正できるようにする #30

--- eikan/models/players.py
+++ eikan/models/players.py
@@ -1,4 +1,4 @@
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 from eikan.models import Teams
 

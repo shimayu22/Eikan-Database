941fe04e56c95c488142f80dbfb58d7087bee751
Yutaka Shimada <shimayu22@gmail.com>
2020-04-03 14:37:38 +0900

    create_game_resultsを１行にした

--- eikan/model_manager.py
+++ eikan/model_manager.py
@@ -4,10 +4,7 @@ class DefaultValueExtractor:
 
 class SavedValueExtractor:
     def create_game_results(self, score, run):
-        if score == run:
-            return 3
-
-        return 1 if score > run else 2
+        return 1 if score > run else 2 if score < run else 3
 
     def update_is_pitcher(self, position, is_pitched):
         return position == 1 or is_pitched

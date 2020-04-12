a7f017ffcf42a29d00f516059c14362a2018f1a3
Yutaka Shimada <shimayu22@gmail.com>
2020-04-12 22:35:32 +0900

    Playersの備考をlist_editableにした

--- eikan/admin.py
+++ eikan/admin.py
@@ -41,7 +41,7 @@ class PlayersAdmin(admin.ModelAdmin):
         'remark')
     list_display = ('name', 'admission_year', 'position', 'is_ob',
                     'is_active', 'is_genius', 'remark')
-    list_editable = ('position',)
+    list_editable = ('position', 'remark',)
 
 
 class FielderResultsInline(admin.TabularInline):

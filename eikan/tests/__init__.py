396aa8f7f2730184a7150e2ba79a15ee5bf439c2
Yutaka Shimada <shimayu22@gmail.com>
2020-05-22 13:53:40 +0900

    テスト作成

--- eikan/tests/__init__.py
+++ eikan/tests/__init__.py
@@ -2,3 +2,4 @@ from eikan.tests.cal_pitcher_sabr_tests import CalculatePitcherSabrTests
 from eikan.tests.cal_fielder_sabr_tests import CalculateFielderSabrTests
 from eikan.tests.cal_team_sabr_tests import CalculateTeamSabrTests
 from eikan.tests.model_manager_tests import DefaultValueExtractorTests, SavedValueExtractorTests, ChoicesFormatterTests
+from eikan.tests.models_tests import TeamsTests, TeamsTotalResultsTests, PlayersTests, FielderResultsTests, FielderTotalResultsTests, PitcherResultsTests, PitcherTotalResultsTests
---
+++ eikan/tests/models_tests.py
@@ -0,0 +1,118 @@
+from django.test import TestCase
+from eikan.models import Teams, Players, Games, FielderResults, FielderTotalResults, PitcherResults, PitcherTotalResults, TeamTotalResults
+from eikan.model_manager import ChoicesFormatter
+
+
+class TeamsTests(TestCase):
+    def test_unique_key_check(self):
+        """(year, period)"""
+        period = ChoicesFormatter.period_choices_to_dict()
+        Teams(year=1985, period=period['夏']).save()
+        Teams(year=1985, period=period['夏']).save()
+        self.assertEqual(Teams.objects.count(), 1)
+        Teams(year=1985, period=period['秋']).save()
+        self.assertEqual(Teams.objects.count(), 2)
+        Teams(year=1986, period=period['夏']).save()
+        self.assertEqual(Teams.objects.count(), 3)
+
+
+class PlayersTests(TestCase):
+    def test_unique_key_check(self):
+        """(addmission_year, name)"""
+        Players(admission_year=1985, name="桑田").save()
+        Players(admission_year=1985, name="桑田").save()
+        self.assertEqual(Players.objects.count(), 1)
+        Players(admission_year=1985, name="清原").save()
+        self.assertEqual(Players.objects.count(), 2)
+        Players(admission_year=2020, name="桑田").save()
+        self.assertEqual(Players.objects.count(), 3)
+
+
+class FielderResultsTests(TestCase):
+    def test_unique_key_check(self):
+        """(game_id, player_id)"""
+        period = ChoicesFormatter.period_choices_to_dict()
+        competition_choices = ChoicesFormatter.competition_choices_to_dict()
+        round_choices = ChoicesFormatter.round_choices_to_dict()
+        # データセット
+        Teams(year=1985, period=period['夏']).save()
+        t1 = Teams.objects.latest('pk')
+        Players(admission_year=1985, name="桑田").save()
+        Players(admission_year=1985, name="清原").save()
+        Games(team_id=t1,
+              competition_type=competition_choices['県大会'],
+              competition_round=round_choices['1回戦'],
+              score=1,
+              run=0).save()
+        g1 = Games.objects.latest('pk')
+        p1 = Players.objects.get(name="桑田")
+        p2 = Players.objects.get(name="清原")
+        FielderResults(game_id=g1, player_id=p1).save()
+        FielderResults(game_id=g1, player_id=p2).save()
+        FielderResults(game_id=g1, player_id=p2).save()
+        self.assertEqual(FielderResults.objects.count(), 2)
+
+
+class PitcherResultsTests(TestCase):
+    def test_unique_key_check(self):
+        """(game_id, player_id)"""
+        period = ChoicesFormatter.period_choices_to_dict()
+        competition_choices = ChoicesFormatter.competition_choices_to_dict()
+        round_choices = ChoicesFormatter.round_choices_to_dict()
+        # データセット
+        Teams(year=1985, period=period['夏']).save()
+        t1 = Teams.objects.latest('pk')
+        Players(admission_year=1985, name="桑田", position=1).save()
+        Players(admission_year=1985, name="清原", position=1).save()
+        Games(team_id=t1,
+              competition_type=competition_choices['県大会'],
+              competition_round=round_choices['1回戦'],
+              score=1,
+              run=0).save()
+        g1 = Games.objects.latest('pk')
+        p1 = Players.objects.get(name="桑田")
+        p2 = Players.objects.get(name="清原")
+        PitcherResults(game_id=g1, player_id=p1).save()
+        PitcherResults(game_id=g1, player_id=p1).save()
+        PitcherResults(game_id=g1, player_id=p2).save()
+        self.assertEqual(PitcherResults.objects.count(), 2)
+
+
+class FielderTotalResultsTests(TestCase):
+    def test_unique_key_check(self):
+        """player_id"""
+        Players(admission_year=1985, name="桑田").save()
+        Players(admission_year=1985, name="清原").save()
+        p1 = Players.objects.get(name="桑田")
+        p2 = Players.objects.get(name="清原")
+        FielderTotalResults(player_id=p1).save()
+        FielderTotalResults(player_id=p2).save()
+        FielderTotalResults(player_id=p2).save()
+        self.assertEqual(FielderTotalResults.objects.count(), 2)
+
+
+class PitcherTotalResultsTests(TestCase):
+    def test_unique_key_check(self):
+        """player_id"""
+        # データセット
+        Players(admission_year=1985, name="桑田", position=1).save()
+        Players(admission_year=1985, name="清原", position=1).save()
+        p1 = Players.objects.get(name="桑田")
+        p2 = Players.objects.get(name="清原")
+        PitcherTotalResults(player_id=p1).save()
+        PitcherTotalResults(player_id=p1).save()
+        PitcherTotalResults(player_id=p2).save()
+        self.assertEqual(PitcherTotalResults.objects.count(), 2)
+
+
+class TeamsTotalResultsTests(TestCase):
+    def test_unique_key_check(self):
+        """team_id"""
+        period = ChoicesFormatter.period_choices_to_dict()
+        Teams(year=1985, period=period['夏']).save()
+        Teams(year=1985, period=period['秋']).save()
+        t1 = Teams.objects.get(year=1985, period=period['夏'])
+        t2 = Teams.objects.get(year=1985, period=period['秋'])
+        TeamTotalResults(team_id=t1).save()
+        TeamTotalResults(team_id=t2).save()
+        self.assertEqual(TeamTotalResults.objects.count(), 2)
